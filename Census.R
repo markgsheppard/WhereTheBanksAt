6e33d4da58c87983ebcd2ddaf538461edbd28fa0
Mark Sheppard <marksheppard@uchicago.edu>
2020-11-18 18:26:03 -0600

    Basic code for a simple state map of income

--- Census.R
+++ Census.R
@@ -1,17 +1,40 @@
 #IPUMS: https://data2.nhgis.org/main
 #https://api.census.gov/data/2018/acs/acs5/variables.html
 #API KEY: https://api.census.gov/data/key_signup.html
-install.packages("tidycensus")
+#https://walker-data.com/tidycensus/articles/basic-usage.html#geography-in-tidycensus
+#install.packages("tidycensus")
+#install.packages("sf")
+#install.packages("tmap")
+#install.packages("usmap")
+#census_api_key("10cde4d62bedba8405563e4ac057760b22a03f07")
 library(tidycensus)
-census_api_key("10cde4d62bedba8405563e4ac057760b22a03f07")
-
+library(tidyverse)
+library(ggplot2)
+library(sf)
+library(tmap)
+library(usmap)
 
 acs_data <- get_acs(
-  geography = "tracts",
+  geography = "state",
+  geometry=TRUE,
   year = 2018,
   survey = "acs5",  #geometry = TRUE,
   variables = c(  
     "B01003_001", # total population
-    "B19013_001" # median household income in the last 12 months (2018 inflation-adjusted dollars)
-  ))
+    "B19013_001" # Median Income
+    ))
 
+acs_data_2 <- acs_data %>% 
+  select(-GEOID,-moe) %>%
+  spread(key = variable, value = estimate) %>%
+  rename("tate"=NAME, "Total Population"=B01003_001, "Median Income" = B19013_001)
+
+
+plot_usmap(
+  data = acs_data_2, values = "Median Income", include = c("California", "Idaho", "Nevada", "Oregon", "Washinton"), color = "red"
+) + 
+  scale_fill_continuous(
+    low = "white", high = "red", name = "Income (2018)", label = scales::comma
+  ) + 
+  labs(title = "Western US States", subtitle = "These are the states in the Pacific Timezone.") +
+  theme(legend.position = "right")
